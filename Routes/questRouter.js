cb1c3471c3c3c0d66988a7c1479c4ce641a4cb4a
himania038 <72872698+himania038@users.noreply.github.com>
2020-10-14 22:17:41 +0530

    Update questRouter.js

--- Routes/questRouter.js
+++ Routes/questRouter.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const questController = require('../Controllers/questController');
 const authController = require('../Controllers/authController');
 
-router.route('/').get(questController.getQuestions);
+router.route('/quest/').get(questController.getQuestions);
 
 router
-  .route('/addQuestion')
+  .route('/quest/addQuestion')
   .post(authController.protect, questController.addQuestion);
 
-router.route('/:id').get(questController.getQuestionById);
-router.route('/?userId').get(questController.getQuestionByUserId);
+router.route('/quest/:id').get(questController.getQuestionById);
+router.route('/quest/?userId').get(questController.getQuestionByUserId);
 
 module.exports = router;
